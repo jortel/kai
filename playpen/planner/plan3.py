c1fb42dae1435253ff21ae0a2faa4fd02ba5b039
Jeff Ortel <jortel@redhat.com>
2024-11-14 11:15:53 -0600

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- playpen/planner/plan3.py
+++ playpen/planner/plan3.py
@@ -4,14 +4,17 @@ import re
 import shutil
 import time
 from os.path import basename
-from typing import List
+from typing import List, Tuple
 
 import tree_sitter
 import tree_sitter_java
+from tree_sitter import Node
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
 from langchain_aws import ChatBedrock
 from tree_sitter_languages.core import Language
+
+from kai.analyzer_types import Report, Incident
 
 pp = pprint.PrettyPrinter(indent=2)
 
@@ -19,97 +22,6 @@ m_id = "meta.llama3-70b-instruct-v1:0"
 m_id = "anthropic.claude-3-5-sonnet-20240620-v1:0"
 llm = ChatBedrock(model_id=m_id, model_kwargs={"temperature": 0.1})
 
-
-issues = """
-### Issue 1
-Issue to fix: "Replace the `javax.ejb` import statement with `jakarta.ejb` "
-Line number: 3
-### Issue 2
-Issue to fix: "Replace the `javax.ejb` import statement with `jakarta.ejb` "
-Line number: 4
-### Issue 3
-Issue to fix: "Replace the `javax.inject` import statement with `jakarta.inject` "
-Line number: 5
-### Issue 4
-Issue to fix: "Replace the `javax.jms` import statement with `jakarta.jms` "
-Line number: 6
-### Issue 5
-Issue to fix: "Replace the `javax.jms` import statement with `jakarta.jms` "
-Line number: 7
-### Issue 6
-Issue to fix: "Replace the `javax.jms` import statement with `jakarta.jms` "
-Line number: 8
-### Issue 7
-Issue to fix: "Replace the `javax.jms` import statement with `jakarta.jms` "
-Line number: 9
-### Issue 8
-Issue to fix: "Enterprise Java Beans (EJBs) are not supported in Quarkus. CDI must be used.
- Please replace the `@MessageDriven` annotation with a CDI scope annotation like `@ApplicationScoped`."
-Line number: 14
-### Issue 9
-Issue to fix: "The `destinationLookup` property can be migrated by annotating a message handler method (potentially `onMessage`) with the
- `org.eclipse.microprofile.reactive.messaging.Incoming` annotation, indicating the name of the queue as a value:
-
- Before:
- ```
- @MessageDriven(name = "HelloWorldQueueMDB", activationConfig = 
- public class MessageListenerImpl implements MessageListener 
- }
- ```
-
- After:
- ```
- public class MessageListenerImpl implements MessageListener 
- }
- ```"
-Line number: 15
-### Issue 10
-Issue to fix: "The `destinationLookup` property can be migrated by annotating a message handler method (potentially `onMessage`) with the
- `org.eclipse.microprofile.reactive.messaging.Incoming` annotation, indicating the name of the queue as a value:
-
- Before:
- ```
- @MessageDriven(name = "HelloWorldQueueMDB", activationConfig = 
- public class MessageListenerImpl implements MessageListener 
- }
- ```
-
- After:
- ```
- public class MessageListenerImpl implements MessageListener 
- }
- ```"
-Line number: 16
-### Issue 11
-Issue to fix: "The `destinationLookup` property can be migrated by annotating a message handler method (potentially `onMessage`) with the
- `org.eclipse.microprofile.reactive.messaging.Incoming` annotation, indicating the name of the queue as a value:
-
- Before:
- ```
- @MessageDriven(name = "HelloWorldQueueMDB", activationConfig = 
- public class MessageListenerImpl implements MessageListener 
- }
- ```
-
- After:
- ```
- public class MessageListenerImpl implements MessageListener 
- }
- ```"
-Line number: 17
-### Issue 12
-Issue to fix: "References to JavaEE/JakartaEE JMS elements should be removed and replaced with their Quarkus SmallRye/Microprofile equivalents."
-Line number: 6
-### Issue 13
-Issue to fix: "References to JavaEE/JakartaEE JMS elements should be removed and replaced with their Quarkus SmallRye/Microprofile equivalents."
-Line number: 7
-### Issue 14
-Issue to fix: "References to JavaEE/JakartaEE JMS elements should be removed and replaced with their Quarkus SmallRye/Microprofile equivalents."
-Line number: 8
-### Issue 15
-Issue to fix: "References to JavaEE/JakartaEE JMS elements should be removed and replaced with their Quarkus SmallRye/Microprofile equivalents."
-Line number: 9
-"""
 
 fetch_prompt = """
 You are an java programming assistant designed to generate json actions.
@@ -320,8 +232,7 @@ class Fetch(Action):
                 patch = Patch(
                     begin=begin,
                     end=end,
-                    code=content,
-                )
+                    code=content)
             case Kind.CLASS:
                 q = f"""
                 ((class_declaration) @constant)
@@ -342,8 +253,7 @@ class Fetch(Action):
                     patch = Patch(
                         begin=begin,
                         end=end,
-                        code=text,
-                    )
+                        code=text)
             case Kind.METHOD:
                 q = f"""
                 ((method_declaration) @constant)
@@ -359,8 +269,7 @@ class Fetch(Action):
                     patch = Patch(
                         begin=begin,
                         end=end,
-                        code=text,
-                    )
+                        code=text)
             case _:
                 print("kind {self.kind} not supported.")
         if patch:
@@ -371,7 +280,8 @@ class Fetch(Action):
 
 class Planner(object):
 
-    def __init__(self, path):
+    def __init__(self, report: Report, path: str):
+        self.report = report
         self.path = path
         java = tree_sitter_java.language()
         self.language = tree_sitter.Language(java)
@@ -381,10 +291,40 @@ class Planner(object):
         tree = self.parser.parse(bytes(content, "utf8"))
         self.root = tree.root_node
 
+    def issues(self) -> str:
+        issues = []
+        n = 1
+        for incident in self.incidents():
+            s = f"### Issue {n}\n"
+            s += f"Issue To Fix: {incident.message}\n"
+            s += f"Line number: {incident.line_number}\n"
+            issues.append(s)
+            n += 1
+        return "\n".join(issues)
+
+    def incidents(self) -> List[Incident]:
+        for ruleset in self.report.rulesets.values():
+            for violation in ruleset.violations.values():
+                for incident in violation.incidents:
+                    yield incident
+
     def predict(self) -> List[Patch]:
-        pass
+        patches = []
+        tree = Tree(self.path, self.root)
+        for incident in self.incidents():
+           found, node = tree.find_at(incident.line_number)
+           if found:
+             patch = tree.patch(node)
+             if patch:
+                 patches.append(patch)
+        collated = {}
+        for p in patches:
+            collated[hash(p.code)] = p
+        patches = list(collated.values())
+        return patches
 
     def fetch(self) -> List[Patch]:
+        issues = self.issues()
         output = self.send(tag="fetch", template=fetch_prompt, issues=issues)
 
         patches = []
@@ -415,6 +355,7 @@ class Planner(object):
             p = "```json\n%s\n```" % json.dumps(d, indent=2)
             part.append(p)
         patches = "\n".join(part)
+        issues = self.issues()
         output = self.send(
             tag="patch", template=patch_prompt, issues=issues, patches=patches
         )
@@ -442,7 +383,6 @@ class Planner(object):
                 print(f"\nREPLACE: >>>{content}<<<")
                 file.seek(0)
                 # DEBUG
-
                 part = [
                     file.read(patch.begin),
                     patch.code,
@@ -482,9 +422,83 @@ class Planner(object):
         print(f"\nDONE (duration={duration:.2f}s)\n")
 
 
+class Tree(object):
+    def __init__(self, path: str, root: Node):
+        self.path = path
+        self.root = root
+
+    def find(self, kind, name: str="", match: str="") -> List[Node]:
+        matched = []
+        for node in self.root.children:
+            if node.type == kind:
+                matched.append(node)
+        return matched
+
+    def first(self, kind, name: str="", match: str="") -> Tuple[bool, Node|None]:
+        matched = self.find(kind, name, match)
+        if len(matched) > 0:
+            return True, matched[0]
+        return False, None
+
+    def find_at(self, line) -> Tuple[bool, Node|None]:
+        def at(node) -> Tuple[bool, Node|None]:
+            found = False
+            row = node.start_point.row + 1
+            if row != line:
+                for child in node.children:
+                    found, node = at(child)
+                    if found:
+                        break
+            else:
+                found = True
+            return found, node
+        return at(self.root)
+
+    def patch(self, node: Node) -> Patch:
+        match node.type:
+            case "import_declaration":
+                matched = self.find(node.type)
+                begin = 0
+                end = 0
+                for node in matched:
+                    if begin == 0:
+                        begin = node.start_byte
+                    end = node.end_byte
+                with open(self.path, "r") as file:
+                    file.seek(begin)
+                    content = file.read(end - begin)
+                return Patch(
+                    begin=begin,
+                    end=end,
+                    code=content)
+            case "class_declaration":
+                begin = node.start_byte
+                end = node.end_byte
+                text = node.text
+                for child in node.children:
+                    if child.type == "class_body":
+                        end = child.start_byte
+                        break
+                text = text[: end - begin]
+                text = text.decode("utf-8")
+                return Patch(
+                    begin=begin,
+                    end=end,
+                    code=text)
+            case "method_declaration":
+                text = node.text.decode("utf-8")
+                return Patch(
+                    begin=node.start_byte,
+                    end=node.end_byte,
+                    code=text)
+            case _:
+                if node.parent:
+                    return self.patch(node.parent)
+
 if __name__ == "__main__":
+    report = Report.load_report_from_file("./input/report.yaml")
     input = "./input/mdb.java"
     output = "./output/" + basename(input)
     shutil.copy(input, output)
-    planner = Planner(path=output)
+    planner = Planner(report=report, path=output)
     planner.plan()
